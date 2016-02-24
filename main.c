7f08cb2c6977868b1a1d30de57b900899ac32cbc
MforMAD <smb.like.you@gmail.com>
2016-02-24 19:29:45 +0600

    Added factor 'a' equal 0 check

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
 	if (argc < 4) {
 		fprintf(stderr, "Missing arguments\n");
-		exit(EXIT_FAILTURE);
+		exit(EXIT_FAILURE);
 	}
 
+	double a = atof(argv[1]);
+
+	if (a == 0) {
+		fprintf(stderr, "Factor 'a' can't be equal to 0\n");
+		exit(EXIT_FAILURE);
+	}
+	
 	return EXIT_SUCCESS;
 }
